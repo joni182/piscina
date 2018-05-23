187b7978d8467ad60ce523155c9b7fd4414da4fa
Jonatan Cerezuela López <jcerezuelalopez@gmail.com>
2018-05-23 19:11:13 +0200

    .sql terminado

--- db/piscina.sql
+++ db/piscina.sql
@@ -2,3 +2,23 @@
 -- Archivo de base de datos --
 ------------------------------
 
+DROP TABLE IF EXISTS usuarios CASCADE;
+
+CREATE TABLE usuarios
+(
+      id bigserial PRIMARY KEY
+    , dni varchar(9) NOT NULL UNIQUE
+    , password varchar(255)
+);
+
+DROP TABLE IF EXISTS reservas CASCADE;
+
+CREATE TABLE reservas
+(
+      dia date NOT NULL
+    , hora time NOT NULL
+    , usuario_id bigint REFERENCES usuarios(id)
+);
+
+INSERT INTO usuarios(dni,password)
+VALUES ('31568412V', crypt('pepe', gen_salt('bf', 11)));
